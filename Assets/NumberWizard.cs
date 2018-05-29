278493f58ae0fdab621064f2b2b1bb2757d299af
jsrj <rj.javadev@icloud.com>
2018-05-28 23:39:56 -0400

    added first welcome line

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,7 +7,8 @@ public class NumberWizard : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         string myName = "Arjay Jones!";
-        Debug.Log(string.Format("Hello, {0}", myName));
+        Debug.Log("Welcome to Number Wizard!");
+        //Debug.Log(string.Format("Hello, {0}", myName));
 	}
 
 	// Update is called once per frame
