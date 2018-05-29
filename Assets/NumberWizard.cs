ddcf3ae7f7cfd6ab288eab84245ec2422da52d12
jsrj <rj.javadev@icloud.com>
2018-05-28 23:43:24 -0400

    added second prompt and number ranges

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -6,8 +6,15 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        string myName = "Arjay Jones!";
+        int min = 1;
+        int max = 1000;
         Debug.Log("Welcome to Number Wizard!");
+        Debug.Log(
+            string.Format("Choose a number between {0} and {1}... \n" +
+                          " I will try to guess this number:",
+            min,
+            max
+        ));
         //Debug.Log(string.Format("Hello, {0}", myName));
 	}
 
