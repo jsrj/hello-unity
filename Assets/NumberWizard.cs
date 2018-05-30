b0d0ce65c8a4c71e8c89ee8bd02848fbb7b4bef6
jsrj <rj.javadev@icloud.com>
2018-05-30 00:15:41 -0400

    added binary sort logic to game

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         int min = 1;
         int max = 1000;
+        int guessedNumber = 0;
         Debug.Log("Welcome to Number Wizard!");
-        Debug.Log(
-            string.Format("Choose a number between {0} and {1}... \n" +
-                          " I will try to guess this number:",
+        Debug.Log(string.Format(
+                "Choose a number between {0} and {1}... \n" +
+                " I will try to guess this number:",
             min,
             max
         ));
-        //Debug.Log(string.Format("Hello, {0}", myName));
-	}
+        while(guessedNumber == 0) {
+            int currentGuess = Random.Range(min, max);
+            int userInput = -1;
+            switch (userInput) {
+                case 1: // Guessed number is higher than user number
+                    max = currentGuess;
+                    break;
+                case 2: // Guessed number is lower than user number
+                    min = currentGuess;
+                    break;
+                case 3: // Guessed number is correct
+                    guessedNumber = currentGuess;
+                    break;    
+            }
+        }
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update () {
+        
+    }
 }
