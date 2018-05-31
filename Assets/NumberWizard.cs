6d6cb641e08ea70ea84e1b45521506f524dad57e
jsrj <rj.javadev@icloud.com>
2018-05-30 22:44:16 -0400

    completed script for game logic

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,59 +4,69 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
+    private int gMin;
+    private int gMax;
+    private int guess;
+    private string userRes;
+
     void guessNumber(int givenMin, int givenMax)
     {
-        int currentGuess = Random.Range(givenMin, givenMax);
-        int currentMin = givenMin;
-        int currentMax = givenMax;
-
-        Debug.Log(string.Format("Is your number {0}? /n" +
-                                    "1: No, its higher than my number. \n" +
-                                    "2: No, its lower than my number. \n" +
-                                    "3: Yes! That is my number!"));
-        int userInput = -1;
-        if ((Input.GetKeyDown(KeyCode.Keypad1)) || (Input.GetKeyDown(KeyCode.Alpha1)))
-        {
-            userInput = 1;
-        }
-        else if ((Input.GetKeyDown(KeyCode.Keypad2)) || (Input.GetKeyDown(KeyCode.Alpha2)))
-        {
-            userInput = 2;
-        }
-        else if ((Input.GetKeyDown(KeyCode.Keypad3)) || (Input.GetKeyDown(KeyCode.Alpha3)))
-        {
-            userInput = 3;
-        }
-        switch (userInput)
-        {
-            case 1: // Guessed number is higher than user number
-                //guessNumber(givenMin, currentGuess);
-                break;
-            case 2: // Guessed number is lower than user number
-                //guessNumber(currentGuess, givenMax);
-                break;
-            case 3: // Guessed number is correct
-                Debug.Log(string.Format("I knew it! {0} is the number you were thinking of!", currentGuess));
-                break;
-        }
+        guess = Random.Range(givenMin, givenMax);
+        gMin = givenMin;
+        gMax = givenMax;
+        Debug.Log("Min: "+gMin);
+        Debug.Log("Max: "+gMax);
+        Debug.Log(string.Format("Is your number [H]igher or [L]ower than {0}? \n" +
+                                    "H: It's higher. \n" +
+                                    "L: It's lower. \n" +
+                                "N: Whoa! That's my number!", guess));
     }
 
     // Use this for initialization
     void Start () {
-        int min = 1;
-        int max = 1000;
+        gMin = 1;
+        gMax = 1000;
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log(string.Format(
                 "Choose a number between {0} and {1}... \n" +
                 " I will try to guess this number:",
-            min,
-            max
+            gMin,
+            gMax
         ));
-        //guessNumber(min, max);
+        userRes = "";
+        guessNumber(gMin, gMax);
     }
 
     // Update is called once per frame
-    void Update () {
-        
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.H)))
+        {
+            userRes = "H";
+        }
+        else if ((Input.GetKeyDown(KeyCode.L)))
+        {
+            userRes = "L";
+        }
+        else if ((Input.GetKeyDown(KeyCode.Keypad3)) || (Input.GetKeyDown(KeyCode.Alpha3)))
+        {
+            userRes = "N";
+        }
+        if (userRes != "")
+        {
+            switch (userRes)
+            {
+                case "H": // Guessed number is higher than user number
+                    guessNumber(guess, gMax);
+                    break;
+                case "L": // Guessed number is lower than user number
+                    guessNumber(gMin, guess);
+                    break;
+                case "N": // Guessed number is correct
+                    Debug.Log(string.Format("I knew it! {0} is the number you were thinking of!", guess));
+                    break;
+            }
+        }
+        userRes = "";
     }
 }
